63d66b4caafc52fd19f17700b3d557ca4a2aa421
ViniRodrig <vmarquesrodrigues25@gmail.com>
2023-06-06 20:06:34 -0300

    We can now exit from our shell using ctrl d or typing exit

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
@@ -7,17 +8,29 @@ int main(int argc, char **argv)
   size_t characters;
   char *command;
   
+
   command = (char *)malloc(bufsize * sizeof(char));
+  char *prompt =  "vish... $ ";
   
   if(command == NULL){
     perror("Unable to allocate buffer");
     exit(1);
   }
+  
+  while(1){
+    printf("%s ", prompt);
+    characters = getline(&command,&bufsize,stdin);
+    
+    // if getline function fail exit
+    if((characters == -1 ) || !(strcmp (command, "exit\n")) ){
+      printf("Exiting shell\n");
+      return(-1);
+    }
+    
+    printf("%s", command)
+;
+  }
 
-  char *prompt =  "vish... $";
-  printf("%s ", prompt);
-    
-  characters = getline(&command,&bufsize,stdin);
-    
+  free(command);
   return 0;
 }
